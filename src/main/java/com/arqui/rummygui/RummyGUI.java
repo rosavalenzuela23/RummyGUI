2eba804d57576979f9ce968ef9b697bcab17ef93
Rosa dorito <natsukiarr@gmail.com>
2024-02-19 07:45:11 -0700

    file start
    primer commit con documentos

---
+++ src/main/java/com/arqui/rummygui/RummyGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.arqui.rummygui;
+
+/**
+ *
+ * @author rosafresita
+ */
+public class RummyGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
